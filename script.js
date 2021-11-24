18a317b0131cb40d6226e4f2e93efc5206c73cce
ACOIDAN MESA HERNANDEZ <ALU0101206479@ULL.EDU.ES>
2021-11-24 21:46:26 +0000

    Ya se puede comparar aunque los vecinos no hayan valorado, ya que lo que hace es pasar al siguiente vecino

--- script.js
+++ script.js
@@ -14,20 +14,28 @@ fichero.addEventListener('change', function(e) {
   reader.readAsText(fichero.files[0]);
 }, false)
 
-  function simple(matriz, u, item, n_vecinos, similitudes) {
+  function simple(matriz, item, n_vecinos, similitudes) {
     if (n_vecinos < 3) {
       alert("ERROR: EL número elegido de vecinos es demasiado pequeño para obtener predicciones con 'Predicción simple'");
     } else {
-      let numerador = 0;
-      let denominador = 0;       
-      for (let k = 0; k < n_vecinos; k++) {
-        numerador = numerador + (similitudes[k][1]*parseInt(matriz[similitudes[k][0]][item]))
-        denominador = denominador + Math.abs(similitudes[k][1])
-      }
 
-      let resultado = parseInt(numerador/denominador).toString();
+        let numerador = 0;
+        let denominador = 0;    
+        // let vecino = 0;   
+        for (let k = 0; k < n_vecinos; k++) {
+          // FALTA COMPROBAR EL ELSE, AQUI LO QUE PASA ES QUE SI ME FIJO EN LA VALORACIÓN DE OTRO USUARIO AL ITEM QUE QUIERO PREDECIR Y ESTA VALORACION ES NULA, PUES QUE HAGA ALGO
+          //if (matriz[similitudes[vecino][0]][item] !== "-") {
+            numerador = numerador + (similitudes[k][1]*parseInt(matriz[similitudes[k][0]][item]))
+            denominador = denominador + Math.abs(similitudes[k][1],matriz[similitudes[3][0]][item])
+          /*} else {
+            vecino++
+          }
+          vecino++;*/
+        }
+        console.log(numerador, denominador, "HOLA", similitudes[3][1], )
+        let resultado = parseInt(numerador/denominador).toString();
 
-      return resultado;
+        return resultado;
     }
   }
 
@@ -59,8 +67,12 @@ fichero.addEventListener('change', function(e) {
 
       let media_v = sumatorio_v/vec_aux2.length
 
-      numerador = numerador + (similitudes[k][1] * (parseInt(matriz[similitudes[k][0]][item]) - media_v))
-      denominador = denominador + Math.abs(similitudes[k][1])
+      // console.log(similitudes[k][0], matriz[similitudes[k][0]][item])
+        // FALTA COMPROBAR EL ELSE, AQUI LO QUE PASA ES QUE SI ME FIJO EN LA VALORACIÓN DE OTRO USUARIO AL ITEM QUE QUIERO PREDECIR Y ESTA VALORACION ES NULA, PUES QUE HAGA ALGO
+      // if (matriz[similitudes[k][0]][item] !== "-") {
+        numerador = numerador + (similitudes[k][1] * (parseInt(matriz[similitudes[k][0]][item]) - media_v))
+        denominador = denominador + Math.abs(similitudes[k][1])
+      // }
     }
 
     let resultado = (media_u + numerador/denominador).toFixed()
@@ -169,7 +181,7 @@ fichero.addEventListener('change', function(e) {
           switch (metrica) {
             case "Correlación de Pearson":
               for (let k = 1; k < matriz_salida.length; k++) {
-                similitud.push([(k+i)%matriz_salida.length, pearson(matriz_salida, i, (k+i)%matriz_salida.length)])
+                similitud.push([(k+i)%matriz_salida.length, pearson(matriz_salida, i, (k+i)%matriz_salida.length)])  
               }
               break
             case "Distancia coseno":
@@ -199,18 +211,55 @@ fichero.addEventListener('change', function(e) {
               }
           });
 
+
           let aux = [[i, j], []]
           switch (tipo_pred) {
             case "Predicción simple":
-              
+              // for (let l = 0; l < similitud.length; l++) {
+              //   console.log(similitud[l]);
+              // }
+              // console.log("Corte")
+              // console.log("SIMILITUD COMPLETO")
+              // for (let k = 0; k < similitud.length; k++) {
+              //   console.log(similitud[k]);
+              // }
               for (let k = 0; k < n_vecinos; k++) {
+                // console.log(similitud[k][0])
+
+                // Con este if me aseguro de que cuando se vaya a hacer la predicción no comprobemos un vecino que tampoco haya valorado ese item, se pasaria al siguiente vecino más cercano
+                if (matriz_salida[similitud[k][0]][j] == "-") {
+                  similitud.splice(k, 1);
+                  k--;
+                  
+                  // console.log("SSSSSSS")
+                  // for (let k = 0; k < similitud.length; k++) {
+                  //   console.log(similitud[k]);
+                  // }
+                  continue;
+
+                }
+                // console.log(similitud[k][0])
                 aux[1].push(similitud[k][0]);
               }
-              matriz_salida[i][j] = simple(matriz_salida, i, j, n_vecinos, similitud)
+              //console.log(simple(matriz_salida, j, n_vecinos, similitud))
+              matriz_salida[i][j] = simple(matriz_salida, j, n_vecinos, similitud)
     
               break
             case "Diferencia con la medida":
+              // console.log("SIMILITUD")
+              // for (let k = 0; k < similitud.length; k++) {
+              //   console.log(similitud[k]);
+              // }
               for (let k = 0; k < n_vecinos; k++) {
+                if (matriz_salida[similitud[k][0]][j] == "-") {
+                  similitud.splice(k, 1);
+                  k--;
+                // console.log("SIMILITUDDDDDDD")
+                // for (let k = 0; k < similitud.length; k++) {
+                //   console.log(similitud[k]);
+                // }
+                  continue;
+                }
                 aux[1].push(similitud[k][0]);
               }
               matriz_salida[i][j] = medida(matriz_salida, i, j, n_vecinos, similitud)
@@ -241,7 +290,7 @@ fichero.addEventListener('change', function(e) {
 
         let salida = "<b>Matriz completa</b><br>"
         let salida_syr = completar_matriz(matriz_entrada, metrica, numero_vecinos, tipo_prediccion)
-      console.log(salida_syr)
+
         for (let i = 0; i < salida_syr[0].length; i++) {
           for (let j = 0; j < salida_syr[0][i].length; j++) {
             if (j > 0) {
@@ -254,26 +303,23 @@ fichero.addEventListener('change', function(e) {
         }
 
         salida = salida + "<br><br><b>Vecinos seleccionados en el proceso de predicción</b><br>"
-console.log(salida_syr[1][0])
+        
         for (let i = 0; i < salida_syr[1].length; i++) {
-          salida = salida + "· Predicción de la calificación (Usuario  " + (parseInt(salida_syr[1][i][0][0])+1) + ", Item " + (parseInt(salida_syr[1][i][0][1])+1) + ")<br>Usuarios de las líneas "
+          salida = salida + "· Predicción de la calificación (Usuario  " + (parseInt(salida_syr[1][i][0][0])+1) + ", Item " + (parseInt(salida_syr[1][i][0][1])+1) + "):<br>Usuarios "
           for (let j = 0; j < salida_syr[1][i][1].length; j++) {
             if (j > 0) {
-              if (j = salida_syr[1][i][1].length-1) {
+              if (j == salida_syr[1][i][1].length-1) {
                 salida = salida + " y " + (parseInt(salida_syr[1][i][1][j])+1)
               } else {
                   salida = salida + ", " + (parseInt(salida_syr[1][i][1][j])+1)
               }
-              
             } else {
               salida = salida + (parseInt(salida_syr[1][i][1][j])+1)
             }
           }
+          salida = salida + "<br>"
         }
         document.getElementById("salida").innerHTML = salida
     }
-
-
-    //console.log(matriz_entrada, )
   }
 
