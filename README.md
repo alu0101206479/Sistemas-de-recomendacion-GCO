ce629b3e070c3245c4e10c0e61a31d69f7e9e94b
ACOIDAN MESA HERNANDEZ <ALU0101206479@ULL.EDU.ES>
2021-11-28 02:32:51 +0000

    Readme hecho a la mitad, codigo limpio y comentado

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Sistemas de recomendación
+## ACOIDAN MESA HERNANDEZ - [alu0101206479@ull.edu.es](alu0101206479@ull.edu.es)
+
+### Explicación del código
+
+Se ha desarrollado un código en ```HTML``` y ```Javascript```, este código consta de un fichero HTML con un formulario con 4 inputs:
+
+  * La selección del fichero que contiene la matriz
+  * La métrica a usar para calcular las similitudes (Coeficiente de Pearson, Distancia Coseno o Distancia Euclídea)
+  * El número de vecinos a usar en la predicción
+  * EL tipo de predicción a utilizar (Predicción simple o Diferencia con la media)
+
+Este formulario consta de un botón que cuando se pulsa llama a una función del codigo de Javascript.
+
+El fichero de Javascript consta de las siguientes funciones:
+
+  * javascript```tratar_datos()```: Función a la que se llama desde el html, recoge los datos y muestra las salidas correspondientes
+  * javascript```completar_matriz(matriz, metrica, num_vecinos, tipo_pred)```: Función principal del programa, esta función es la que llama a todas las demás y la que se encarga de completar la matriz de predicciones, calcular todas las similitudes, todas las predicciones...
+  * javascript```calculo_medias(matriz, i)```: Función que calcula las medias de las calificaciones de los items de todos los usuarios de la matriz y las retorna en forma de vector
+  * javascript```coeficiente_pearson(matriz, medias, usuario1, usuario2)```: Función que calcula las similitud entre dos usuarios a través del "Coeficiente de Pearson"
+  * javascript```coseno(matriz, usuario1, usuario2)```: Función que calcula la similitud entre dos usuarios a través de la "Distancia Coseno"
+  * javascript```euclidea(matriz, usuario1, usuario2)```: Función que calcula la similitud entre dos usuarios a través de la "Distancia Euclídea"
+  * javascript```simple(matriz, item, num_vecinos, similitudes)```: Función que calcula la predicción de la calificación de un usuario a un item con tipo de predicción "Simple"
+  * javascript```dif_media(matriz, medias, u, item, num_vecinos,  similitudes)```: Función que calcula la predicción de la calificación de un usuario a un item con tipo de predicción "Diferencia con la media"
+
+A parte de estas funciones en el fichero de Javascript también hay un manejador para el evento ```change``` del input en el que se selecciona el fichero que contiene la matriz para obtener dicha matriz de este y almacenarla en una variable.
+
+### Ejemplo de uso
+
+Parametros de entrada:
+
+  * Fichero: Seleccionar un fichero que contenga la siguiente matriz (Nos tenemos que asegurar que esta solo contenga espacios entre medio de los números, y no espacios ni al final ni al principio de cada fila):
+    5 3 4 4 -
+    3 1 2 3 3
+    4 3 4 3 5
+    3 3 1 5 4
+    1 5 5 2 1
+  * Métrica: Correlación de Pearson
+  * Número de vecinos: 2
+  * Tipo de predicción: Diferencia con la media
+
+Salida:
+
+
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Leo el fichero
 var fichero = document.getElementById('fichero')
 var matriz_entrada = []
 
+// Almaceno en matriz_entrada la matriz contenida en el fichero
 fichero.addEventListener('change', function(e) {
   matriz_entrada = []
   let reader = new FileReader();
@@ -15,6 +17,7 @@ fichero.addEventListener('change', function(e) {
   reader.readAsText(fichero.files[0]);
 }, false)
 
+  // Función que calcula las medias de las calificaciones de los items de todos los usuarios de la matriz y las retorna en forma de vector
   function calculo_medias(matriz, i) {
     let medias = []
     
@@ -38,15 +41,19 @@ fichero.addEventListener('change', function(e) {
     return medias;
   }
 
-  function simple(matriz, item, n_vecinos, similitudes) {
-    if (n_vecinos < 3) {
-      alert("ERROR: EL número elegido de vecinos es demasiado pequeño para obtener predicciones con 'Predicción simple'");
+  // Función que calcula la predicción de la calificación de un usuario a un item con tipo de predicción "Simple"
+  function simple(matriz, item, num_vecinos, similitudes) {
+    // Se necesitan un número mínimo de k vecinos para la predicción simple (No menos de 3)
+    if (num_vecinos < 3) {
+      alert("ERROR: EL número elegido de vecinos es demasiado pequeño para obtener predicciones con 'Predicción simple', no pueden seleccinarse menos de 3 vecinos");
+      throw new Error();
     } else {
 
         let numerador = 0;
         let denominador = 0;    
-        // let vecino = 0;   
-        for (let k = 0; k < n_vecinos; k++) {
+
+        // Calculo de formula
+        for (let k = 0; k < num_vecinos; k++) {
             numerador = numerador + (similitudes[k][1]*parseInt(matriz[similitudes[k][0]][item]))
             denominador = denominador + Math.abs(similitudes[k][1],matriz[similitudes[3][0]][item])
         }
@@ -57,20 +64,13 @@ fichero.addEventListener('change', function(e) {
   }
 
 
-
-  function dif_media(matriz, medias, u, item, n_vecinos,  similitudes) {
+  // Función que calcula la predicción de la calificación de un usuario a un item con tipo de predicción "Diferencia con la media"
+  function dif_media(matriz, medias, u, item, num_vecinos,  similitudes) {
     let numerador = 0;
     let denominador = 0;
-    console.log(medias);
-    let vec_aux = matriz[u].filter(function(x) {
-      return x !== "-";
-    })
 
-    for (let k = 0; k < n_vecinos; k++) {
-      let vec_aux2 = matriz[similitudes[k][0]].filter(function(x) {
-        return x !== "-";
-      })
-
+    // Calculo de formula
+    for (let k = 0; k < num_vecinos; k++) {
       numerador = numerador + (similitudes[k][1] * (parseInt(matriz[similitudes[k][0]][item]) - medias[similitudes[k][0]]))
       denominador = denominador + Math.abs(similitudes[k][1])
     }
@@ -81,12 +81,13 @@ fichero.addEventListener('change', function(e) {
   }
 
 
-
-  function pearson(matriz, medias, vecino1, vecino2) {
+  // Función que calcula las similitud entre dos usuarios a través del "Coeficiente de Pearson"
+  function coeficiente_pearson(matriz, medias, usuario1, usuario2) {
     let vec_aux = []
 
-    for (let i = 0; i < matriz[vecino1].length; i++) {
-      if (matriz[vecino1][i] !== "-" && matriz[vecino2][i] !== "-") {
+    // Obtengo las posiciones de los items calificados por ambos usuarios para posteriormente hacer el sumatorio con los items calificados por los dos
+    for (let i = 0; i < matriz[usuario1].length; i++) {
+      if (matriz[usuario1][i] !== "-" && matriz[usuario2][i] !== "-") {
         vec_aux.push(i)
       }
     }
@@ -94,11 +95,13 @@ fichero.addEventListener('change', function(e) {
     let numerador = 0
     let den_1 = 0
     let den_2 = 0
-    for (let i = 0; i < vec_aux.length; i++) {
-      numerador = numerador + ((parseInt(matriz[vecino1][vec_aux[i]])-medias[vecino1])*(parseInt(matriz[vecino2][vec_aux[i]])-medias[vecino2]))
 
-      den_1 = den_1 + Math.pow(parseInt(matriz[vecino1][vec_aux[i]])-medias[vecino1],2)
-      den_2 = den_2 + Math.pow(parseInt(matriz[vecino2][vec_aux[i]])-medias[vecino2],2)
+    // Calculo de formulas
+    for (let i = 0; i < vec_aux.length; i++) {
+      numerador = numerador + ((parseInt(matriz[usuario1][vec_aux[i]])-medias[usuario1])*(parseInt(matriz[usuario2][vec_aux[i]])-medias[usuario2]))
+
+      den_1 = den_1 + Math.pow(parseInt(matriz[usuario1][vec_aux[i]])-medias[usuario1],2)
+      den_2 = den_2 + Math.pow(parseInt(matriz[usuario2][vec_aux[i]])-medias[usuario2],2)
     }
 
     den_1 = Math.sqrt(den_1)
@@ -112,11 +115,13 @@ fichero.addEventListener('change', function(e) {
   }
 
 
-
-  function coseno(matriz, vecino1, vecino2) {
+  // Función que calcula la similitud entre dos usuarios a través de la "Distancia Coseno"
+  function coseno(matriz, usuario1, usuario2) {
     let vec_aux = []
-    for (let i = 0; i < matriz[vecino1].length; i++) {
-      if (matriz[vecino1][i] != "-" && matriz[vecino2][i] != "-") {
+
+    // Obtengo las posiciones de los items calificados por ambos usuarios para posteriormente hacer el sumatorio con los items calificados por los dos
+    for (let i = 0; i < matriz[usuario1].length; i++) {
+      if (matriz[usuario1][i] != "-" && matriz[usuario2][i] != "-") {
         vec_aux.push(i)
       }
     }
@@ -124,11 +129,13 @@ fichero.addEventListener('change', function(e) {
     let numerador = 0
     let den_1 = 0
     let den_2 = 0
-    for (let i = 0; i < vec_aux.length; i++) {
-      numerador = numerador+(parseInt(matriz[vecino1][vec_aux[i]])*parseInt(matriz[vecino2][vec_aux[i]]))
 
-      den_1 = den_1+Math.pow(parseInt(matriz[vecino1][vec_aux[i]]), 2)
-      den_2 = den_2+Math.pow(parseInt(matriz[vecino2][vec_aux[i]]), 2)
+    // Calculo de formulas
+    for (let i = 0; i < vec_aux.length; i++) {
+      numerador = numerador+(parseInt(matriz[usuario1][vec_aux[i]])*parseInt(matriz[usuario2][vec_aux[i]]))
+
+      den_1 = den_1+Math.pow(parseInt(matriz[usuario1][vec_aux[i]]), 2)
+      den_2 = den_2+Math.pow(parseInt(matriz[usuario2][vec_aux[i]]), 2)
     }
 
     den_1 = Math.sqrt(den_1)
@@ -141,19 +148,22 @@ fichero.addEventListener('change', function(e) {
   }
 
 
-
-  function euclidea(matriz, vecino1, vecino2) {
+  // Función que calcula la similitud entre dos usuarios a través de la "Distancia Euclídea"
+  function euclidea(matriz, usuario1, usuario2) {
     let vec_aux = []
-    for (let i = 0; i < matriz[vecino1].length; i++) {
-      if (matriz[vecino1][i] !== "-" && matriz[vecino2][i] !== "-") {
+
+    // Obtengo las posiciones de los items calificados por ambos usuarios para posteriormente hacer el sumatorio con los items calificados por los dos
+    for (let i = 0; i < matriz[usuario1].length; i++) {
+      if (matriz[usuario1][i] !== "-" && matriz[usuario2][i] !== "-") {
         vec_aux.push(i)
       }
     }
     
     let resultado = 0
+
+    // Calculo de formula
     for (let i = 0; i < vec_aux.length; i++) {
-      console.log(resultado)
-      resultado = resultado+Math.pow((parseInt(matriz[vecino1][vec_aux[i]])-parseInt(matriz[vecino2][vec_aux[i]])), 2)
+      resultado = resultado+Math.pow((parseInt(matriz[usuario1][vec_aux[i]])-parseInt(matriz[usuario2][vec_aux[i]])), 2)
     }
 
     resultado = parseFloat(Math.sqrt(resultado).toFixed(2))
@@ -162,30 +172,33 @@ fichero.addEventListener('change', function(e) {
   }
   
 
-  
-  function completar_matriz(matriz, metrica, n_vecinos, tipo_pred) {
-    let matriz_salida = JSON.parse(JSON.stringify(matriz));
-    let similiaridades = [];
-    let vecinos_seleccionados = [];
-    let calculo_predicciones = [];
-    let medias_usuarios = calculo_medias(matriz);
+  // Función principal del programa, esta función es la que llama a todas las demás y la que se encarga de completar la matriz de predicciones, calcular todas las similitudes, todas las predicciones...
+  function completar_matriz(matriz, metrica, num_vecinos, tipo_pred) {
+    let matriz_salida = JSON.parse(JSON.stringify(matriz)); // Matriz que se va a completar, copia de la matriz original
+    let similiaridades = [];  // Similiaridades entre todos los pares de usuarios en un Vector de vectores con formato [[Pos. usuario u, [Pos. usuario v, Similitud entre ellos]..]...]
+    let vecinos_seleccionados = []; // Vector que contendrá los vecinos seleccionados en cada predicción en vectores con formato [Pos. usuario, Pos. item, [Pos. vecino X, Pos. Vecino Y..]..]
+    let calculo_predicciones = [];  // Vector que contendrá los resultados de cada predicción en un vector de vectores con formato [[Pos. usuario, Pos. Item, Calc. Predicción]..]
+    let medias_usuarios = calculo_medias(matriz); // Vector con todas las medias de los usuarios
 
+    // Calculo de las similiaridades entre todos los usuarios según la metrica elegida
     for (let i = 0; i < matriz.length; i++) {
       for (let j = 0; j < matriz.length; j++) {
         if (i !== j) {
           let indice = 0;
           switch (metrica) {
             case "Correlación de Pearson":
+              
               indice = similiaridades.findIndex((elemento) => {
                 if (elemento[0] === i) {
                   return true;
                 }
               })
 
+              // Se evalúa si ya en el vector se encuentra el usuario (Ya se ha introducido la similitud con algun otro usuario)
               if (indice === -1) {
-                similiaridades.push([i, [[j, pearson(matriz, medias_usuarios, i, j)]]]);
+                similiaridades.push([i, [[j, coeficiente_pearson(matriz, medias_usuarios, i, j)]]]); // Se introduce por primera vez el usuario y su similitud con otro usuario
               } else {
-                similiaridades[indice][1].push([j, pearson(matriz, medias_usuarios, i, j)])
+                similiaridades[indice][1].push([j, coeficiente_pearson(matriz, medias_usuarios, i, j)]) // Se introduce la similitud con otro usuario más
               }
               
               break
@@ -216,8 +229,8 @@ fichero.addEventListener('change', function(e) {
               }
               break
             case "default":
-                console.error("ERROR: No se ha proporcionado ninguna metrica valida")
-                console.log("Metricas validas = [pearson, coseno, euclidea]")
+                alert("Ha habido un error en la elección de la métrica")
+                throw new Error();
               break
           }
         }
@@ -228,15 +241,15 @@ fichero.addEventListener('change', function(e) {
     for (let i = 0; i < matriz.length; i++) {
       for (let j = 0; j < matriz[i].length; j++) {
         if (matriz[i][j] == "-") {
-
           let indice = similiaridades.findIndex((elemento) => {
             if (elemento[0] === i) {
               return true;
             }
           })
 
-          let simil_vecinos = JSON.parse(JSON.stringify(similiaridades[indice][1]));
+          let simil_vecinos = JSON.parse(JSON.stringify(similiaridades[indice][1])); // Vector que contendrá la similitud del usuario que tiene el item sin calificar con sus vecinos
 
+          // Orden de las similitudes según la métrica
           switch (metrica) {
             case "Correlación de Pearson":
               simil_vecinos.sort(function (a, b) {
@@ -250,8 +263,7 @@ fichero.addEventListener('change', function(e) {
                       }
                   }
               });
-              console.log(simil_vecinos);
-            break
+              break
             case "Distancia coseno":
               for (let k = 1; k < matriz.length; k++) {
                 simil_vecinos.sort(function (a, b) {
@@ -281,23 +293,22 @@ fichero.addEventListener('change', function(e) {
               });
               break
             case "default":
-                console.error("ERROR: No se ha proporcionado ninguna metrica valida")
-                console.log("Metricas validas = [pearson, coseno, euclidea]")
+              alert("Ha habido un error en la elección de la métrica")
+              throw new Error();
               break
           }
 
-          console.log(simil_vecinos)
-          let aux = [i, j, []]
+
+          let aux = [i, j, []] // Vector auxiliar para almacenar los vecinos seleccionados en el calculo de la predicción del usuario i con el item j
           let prediccion = 0
           switch (tipo_pred) {
             case "Predicción simple":
-              for (let k = 0; k < n_vecinos; k++) {
-                // Con este if me aseguro de que cuando se vaya a hacer la predicción no comprobemos un vecino que tampoco haya valorado ese item, se pasaria al siguiente vecino más cercano
+              // For para pushear los vecinos que vamos a utilizar en el vector
+              for (let k = 0; k < num_vecinos; k++) {
+                // Con este if nos aseguramos de que no se utilice un vecino que no haya calificado ese item para el calculo de la predicción, en el caso de que sea así se saca este vecino del vector y se pasa al siguiente (A no ser que no queden más que en ese caso la predicción se calcularía con un vecino menos)
                 if (matriz[simil_vecinos[k][0]][j] == "-") {
                   simil_vecinos.splice(k, 1);
                   k--;
-                  console.log("aaaa")
-                  console.log(simil_vecinos);
                   continue;
                   
                 }
@@ -305,14 +316,14 @@ fichero.addEventListener('change', function(e) {
                 aux[2].push(simil_vecinos[k][0]);
               }
               
-              prediccion = simple(matriz, j, n_vecinos, simil_vecinos)
-              calculo_predicciones.push([i, j, prediccion])
-              matriz_salida[i][j] = parseInt(prediccion).toFixed();
+              prediccion = simple(matriz, j, num_vecinos, simil_vecinos) // Calculo de predicción
+              calculo_predicciones.push([i, j, prediccion]) // Introducimos el calculo en el vector en formato [usuario, item, valorPrediccion]
+              matriz_salida[i][j] = parseInt(prediccion).toFixed(); // Cambiamos el guión por la calificación predecida por ese item en la matriz de utilidad a completar
     
               break
 
             case "Diferencia con la media":
-              for (let k = 0; k < n_vecinos; k++) {
+              for (let k = 0; k < num_vecinos; k++) {
                 if (matriz[simil_vecinos[k][0]][j] == "-") {
                   simil_vecinos.splice(k, 1);
                   k--;
@@ -321,17 +332,17 @@ fichero.addEventListener('change', function(e) {
                 aux[2].push(simil_vecinos[k][0]);
               }
 
-              prediccion = dif_media(matriz, medias_usuarios, i, j, n_vecinos, simil_vecinos)
+              prediccion = dif_media(matriz, medias_usuarios, i, j, num_vecinos, simil_vecinos)
               calculo_predicciones.push([i, j, prediccion])
               matriz_salida[i][j] = parseInt(prediccion).toFixed();
               break
             case "default":
-                console.error("ERROR: No se ha proporcionado ninguna metrica valida")
-                console.log("Metricas validas = [pearson, coseno, euclidea]")
+              alert("Ha habido un error en la elección de la métrica")
+              throw new Error();
               break
           }
           
-          vecinos_seleccionados.push(aux);
+          vecinos_seleccionados.push(aux); // Una vez todos los vecinos seleccionados han sido pusheados en el vector auxiliar, lo introducimos en el vector que contendrá los vecinos seleccionados en cada predicción
         }
       }
     }
@@ -339,18 +350,29 @@ fichero.addEventListener('change', function(e) {
     return [matriz_salida, similiaridades, vecinos_seleccionados, calculo_predicciones];
   }
 
-  function recoger_informacion() {
 
-    if (matriz_entrada.length == 0) {
+
+  // Función a la que se llama desde el html, recoge los datos y muestra la salida
+  function tratar_datos() {
+    if (matriz_entrada.length == 0) { // Comprobamos si la matriz se ha recogido del fichero correctamente
       alert("La matriz no se ha podido procesar correctamente");
-    } else {
+      throw new Error();
+    } else { 
+      numero_vecinos = document.getElementById("numero_vecinos").value
+
+      let alerta = "Se está escogiendo un número de vecinos mayor que usuarios existentes (Número de vecinos: " + numero_vecinos.toString() + ", Número máximo de vecinos posibles: " + (matriz_entrada.length-1).toString() +")"
+      if (numero_vecinos > matriz_entrada.length-1) { // Comprobamos si el número de vecinos escrito es posible
+        alert(alerta);
+        throw new Error();
+      } else {
+
         metrica = document.getElementById("metrica").value
-        numero_vecinos = document.getElementById("numero_vecinos").value
         tipo_prediccion = document.getElementById("tipo_prediccion").value
 
         let salida = "<b>Matriz completa</b><br>"
-        let salida_syr = completar_matriz(matriz_entrada, metrica, numero_vecinos, tipo_prediccion)
+        let salida_syr = completar_matriz(matriz_entrada, metrica, numero_vecinos, tipo_prediccion) // Realizamos la función del sistema de recomendación
 
+        // Ajustamos la salida y la mostramos por pantalla
         for (let i = 0; i < salida_syr[0].length; i++) {
           for (let j = 0; j < salida_syr[0][i].length; j++) {
             if (j > 0) {
@@ -381,7 +403,7 @@ fichero.addEventListener('change', function(e) {
         }
         
         salida = salida + "<br><br><b>Vecinos seleccionados en el proceso de predicción</b><br>"
-        console.log(salida_syr[2])
+
         for (let i = 0; i < salida_syr[2].length; i++) {
           salida = salida + "· Predicción de la calificación Usuario " + (parseInt(salida_syr[2][i][0])+1) + "- Item " + (parseInt(salida_syr[2][i][1])+1) + "):<br>Usuarios "
           for (let j = 0; j < salida_syr[2][i][2].length; j++) {
@@ -399,13 +421,14 @@ fichero.addEventListener('change', function(e) {
         }
 
         salida = salida + "<br><br><b>Cálculo de cada predicción de la matriz de utilidad en base a los vecinos seleccionados</b><br>"
-        console.log(salida_syr[2])
+
         for (let i = 0; i < salida_syr[3].length; i++) {
           salida = salida + "· Predicción de la calificación Usuario " + (parseInt(salida_syr[3][i][0])+1) + "- Item " + (parseInt(salida_syr[3][i][1])+1) + ": <b>" + salida_syr[3][i][2] + "</b><br>"
         }
 
         salida = salida + "<br><br>"
         document.getElementById("salida").innerHTML = salida
+      }
     }
   }
 
